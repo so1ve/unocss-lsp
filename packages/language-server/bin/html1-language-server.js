c73c7d122ad9e3810dbd08d39d920c2b4986171c
Ray <i@mk1.io>
2023-09-05 15:59:15 +0800

    Initial commit

---
+++ packages/language-server/bin/html1-language-server.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+if (process.argv.includes("--version")) {
+	const packageJSON = require("../package.json");
+	// eslint-disable-next-line no-console
+	console.log(`${packageJSON.version}`);
+} else {
+	require("../dist/index.js");
+}
